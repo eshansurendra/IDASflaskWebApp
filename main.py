3e745469b7108d024d7476b6c6431903b5bef722
Januka Eshan Surendra <107266456+eshansurendra@users.noreply.github.com>
2024-02-06 21:29:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+@app.route('/save', methods=['POST'])
+def save_to_file():
+    data = request.json  # assuming JSON data is sent in the request
+    if data:
+        with open('text.txt', 'a') as file:
+            file.write(str(data) + '\n')
+        return 'Data saved successfully!', 200
+    else:
+        return 'No data received!', 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
